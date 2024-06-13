1f1809d49aa6365b8bb0daeefcfdfd5cfc0c3bb9
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-06-13 23:39:46 +0300

    REST_PathVariable

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/anke/demo/rest/Student_REST_Controller.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/anke/demo/rest/Student_REST_Controller.java
@@ -1,7 +1,9 @@
 package com.anke.demo.rest;
 
 import com.anke.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +14,26 @@ import java.util.List;
 @RequestMapping("/api")
 public class Student_REST_Controller {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    private List<Student> students;
 
-        List<Student> students = new ArrayList<>();
+    @PostConstruct
+    public void loadData(){
+
+        students = new ArrayList<>();
+
         students.add(new Student("John", "Doe"));
         students.add(new Student("Mario", "Rossi"));
         students.add(new Student("Mary", "Smith"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        // just index into the list ... keep it simple for now
+        return students.get(studentId);
     }
 }
