d6852e39f6c20821aa11c2c63d15ee9d8b5b701f
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-06-14 06:06:45 +0300

    REST_resource path

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/anke/jpacrud/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/anke/jpacrud/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.anke.jpacrud.dao;
 
 import com.anke.jpacrud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 }
