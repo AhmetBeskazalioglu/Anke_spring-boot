7345bba19d5cadb2d09805075aa74317f44c84c2
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-06-15 06:06:03 +0300

    Spring-Boot-REST-Security-InMemeoryUserDetailsManager and UserDetais interface

--- 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/anke/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/anke/cruddemo/security/DemoSecurityConfig.java
@@ -2,14 +2,61 @@ package com.anke.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // add support for JDBC... no more hardcoding users
     @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?");
+        // define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?");
+        return jdbcUserDetailsManager;
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer -> configurer
+                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/api/employees").hasAnyRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/employees").hasAnyRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        // use httpBasic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable cross side request forgery (CSRF)
+        // CSRF saldırıları, kullanıcıların tarayıcıları üzerinden yetkisiz işlemler yapılmasına olanak tanır.
+        // in general, not required for stateless REST APIs that use POST, PUT, DELETE, etc.
+        http.csrf(AbstractHttpConfigurer::disable);
+        // or
+        // http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+    /*    @Bean
     // InMemoryUserDetailsManager kullanıcı bilgilerini hafızada tutar.
     public InMemoryUserDetailsManager userDetailsService() {
 
@@ -20,18 +67,18 @@ public class DemoSecurityConfig {
                 .roles("EMPLOYEE")
                 .build();
 
-        UserDetails mary= User.builder()
+        UserDetails mary = User.builder()
                 .username("mary")
                 .password("{noop}test123")
-                .roles("EMPLOYEE","MANAGER")
+                .roles("EMPLOYEE", "MANAGER")
                 .build();
 
-        UserDetails susan= User.builder()
+        UserDetails susan = User.builder()
                 .username("susan")
                 .password("{noop}test123")
-                .roles("EMPLOYEE","ADMIN","MANAGER")
+                .roles("EMPLOYEE", "ADMIN", "MANAGER")
                 .build();
 
-        return new InMemoryUserDetailsManager(john,mary,susan);
-    }
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }*/
 }
