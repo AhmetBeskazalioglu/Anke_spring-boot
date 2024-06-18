4a55c784f4a0cfd46dd335444569defebc90ba8c
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-06-18 22:58:59 +0300

    REST without controller, REST with controller,

---
+++ 07-spring-boot-spring-mvc-crud/SpringBoot_Employee_Thymeleaf/src/main/java/com/anke/jpacrud/dao/Rest_InstanceRepository.java
@@ -0,0 +1,7 @@
+package com.anke.jpacrud.dao;
+
+import com.anke.jpacrud.entity.Rest_Instance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Rest_InstanceRepository extends JpaRepository<Rest_Instance, Integer> {
+}
