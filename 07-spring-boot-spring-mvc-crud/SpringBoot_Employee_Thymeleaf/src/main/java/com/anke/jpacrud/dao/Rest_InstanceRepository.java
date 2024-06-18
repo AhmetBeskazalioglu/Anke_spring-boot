8c16d99d8225d8e8f811203c1ec913861e4d4b3c
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-06-18 23:17:52 +0300

    Security with Rest with(out) controller

--- 07-spring-boot-spring-mvc-crud/SpringBoot_Employee_Thymeleaf/src/main/java/com/anke/jpacrud/dao/Rest_InstanceRepository.java
+++ 07-spring-boot-spring-mvc-crud/SpringBoot_Employee_Thymeleaf/src/main/java/com/anke/jpacrud/dao/Rest_InstanceRepository.java
@@ -2,6 +2,8 @@ package com.anke.jpacrud.dao;
 
 import com.anke.jpacrud.entity.Rest_Instance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface Rest_InstanceRepository extends JpaRepository<Rest_Instance, Integer> {
 }
