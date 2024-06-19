71783508af4faa5b67194b408b1511d8bb27b8ed
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-06-19 20:04:51 +0300

    one to one

---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/anke/OneToOne/OneToOneApplication.java
@@ -0,0 +1,119 @@
+package com.anke.OneToOne;
+
+import com.anke.OneToOne.dao.AppDAO;
+import com.anke.OneToOne.entity.Instructor;
+import com.anke.OneToOne.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class OneToOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneToOneApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(AppDAO appDAO) {
+
+        return runner -> {
+            // createInstructor(appDAO);
+
+            // findInstructor(appDAO);
+
+            // deleteInstructor(appDAO);
+
+            // findInstructorDetail(appDAO);
+
+            deleteInstructorDetail(appDAO);
+        };
+    }
+
+    private void deleteInstructorDetail(AppDAO appDAO) {
+
+        int theId = 2;
+        System.out.println("Deleting instructor detail id: " + theId);
+
+        appDAO.deleteInstructorDetailById(theId);
+
+        System.out.println("Done!");
+    }
+
+    private void findInstructorDetail(AppDAO appDAO) {
+
+        // get the instructor detail object
+        int theId = 2;
+        InstructorDetail tempInstructorDetail = appDAO.findInstructorDetailById(theId);
+
+        // print the instructor detail
+        System.out.println("tempInstructorDetail: " + tempInstructorDetail);
+
+        // print the associated instructor
+        System.out.println("the associated instructor: " + tempInstructorDetail.getInstructor());
+
+        System.out.println("Done!");
+    }
+
+    private void deleteInstructor(AppDAO appDAO) {
+
+        int theId = 1;
+        System.out.println("Deleting instructor id: " + theId);
+
+        appDAO.deleteInstructorById(theId);
+
+        System.out.println("Done!");
+    }
+
+    private void findInstructor(AppDAO appDAO) {
+
+        int theId = 2;
+        System.out.println("Finding instructor id: " + theId);
+
+        Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+        System.out.println("tempInstructor: " + tempInstructor);
+        System.out.println("the associated instructorDetail only: " + tempInstructor.getInstructorDetail());
+
+    }
+
+    private void createInstructor(AppDAO appDAO) {
+
+		/*
+		// create the instructor
+		Instructor tempInstructor =
+				new Instructor("Ahmet", "Beskazalioglu", "ahmet@ahmet.com");
+
+		// create the instructor detail
+		InstructorDetail tempInstructorDetail =
+				new InstructorDetail(
+						"http://www.ahmetbeskazalioglu.com/youtube",
+						"Anke!!!");
+		*/
+
+        // create the instructor
+        Instructor tempInstructor =
+                new Instructor("Kerem", "Bekazalioglu", "kerem@kerem.com");
+
+        // create the instructor detail
+        InstructorDetail tempInstructorDetail =
+                new InstructorDetail(
+                        "http://www.kerembeskazalioglu.com/youtube",
+                        "Football");
+
+        // associate the objects
+        tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+        // save the instructor
+        //
+        // NOTE: this will ALSO save the details object
+        // because of CascadeType.ALL
+        //
+        System.out.println("Saving instructor: " + tempInstructor);
+        appDAO.save(tempInstructor);
+
+        System.out.println("Done!");
+    }
+
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/anke/OneToOne/entity/Instructor.java
@@ -0,0 +1,36 @@
+package com.anke.OneToOne.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+@ToString
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @NonNull
+    @Column(name = "first_name")
+    private String firstName;
+
+    @NonNull
+    @Column(name = "last_name")
+    private String lastName;
+
+    @NonNull
+    @Column(name = "email")
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "instructor_detail_id")
+    private InstructorDetail instructorDetail;
+}
